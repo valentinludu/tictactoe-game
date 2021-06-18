6af046b7ffd51db64c4fede243cd4add5a2fc175
valentinludu <ludu.valentin@yahoo.com>
2021-06-18 19:14:19 +0300

    fix: Test

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -3,6 +3,6 @@ import { render } from '@testing-library/react';
 import Game from './Game';
 
 test('renders Game component', () => {
-  const { container } = render(<Game />);
+  const { container } = render(<Game sendWinner={() => {}} players={{ player1: "Me", player2: "You" }} />);
   expect(container).toBeInTheDocument();
 });
